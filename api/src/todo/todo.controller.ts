f9bfebb4057c0b57af62c72539a45dfe3eec9e2c
mariefj <marie.forja@gmail.com>
2023-04-09 14:58:36 +0200

    feat(): add get todo by id

--- api/src/todo/todo.controller.ts
+++ api/src/todo/todo.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express'
 
 import { TodoModel } from './todo.model'
+import mongoose from 'mongoose'
 
 export const getTodos = async (_req: Request, res: Response) => {
 	try {
@@ -8,5 +9,23 @@ export const getTodos = async (_req: Request, res: Response) => {
 		res.status(200).json(todos)
 	} catch (error) {
 		res.status(500).json({ message: 'Error retrieving todos' })
+	}
+}
+
+export const getTodoById = async (req: Request, res: Response) => {
+	try {
+		const id = req.params.id
+		if (!mongoose.isValidObjectId(id)) {
+			res.status(400).json({ message: 'Id not valid' })
+			return
+		}
+		const todo = await TodoModel.findById(id)
+		if (!todo) {
+			res.status(404).json({ message: 'Todo not found' })
+			return
+		}
+		res.status(200).json(todo)
+	} catch (error) {
+		res.status(500).json({ message: 'Error retrieving todo' })
 	}
 }
--- api/src/todo/todo.routes.ts
+++ api/src/todo/todo.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 
-import { getTodos } from './todo.controller'
+import { getTodoById, getTodos } from './todo.controller'
 
 const router = Router()
 
 // Get all todos
 router.get('/', getTodos)
+
+// Get todo
+router.get('/:id', getTodoById)
 
 export { router }
