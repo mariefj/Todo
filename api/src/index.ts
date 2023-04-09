190f1c97afb43174a67687a91272063d880e0c0b
mariefj <marie.forja@gmail.com>
2023-04-09 15:16:26 +0200

    feat(): add body-parser

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express'
+import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 
 import { todoRouter } from './todo'
@@ -6,6 +7,7 @@ import { todoRouter } from './todo'
 const dbUrl = 'mongodb://172.17.0.2:27017/todos'
 
 const app: Application = express()
+app.use(bodyParser.json())
 
 app.use('/todos', todoRouter)
 
