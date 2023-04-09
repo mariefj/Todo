caf8c99d87d35ef83be1f68f2d9c832401afa0be
mariefj <marie.forja@gmail.com>
2023-04-09 16:15:22 +0200

    feat(): add validation body and id routes

--- api/src/todo/todo.routes.ts
+++ api/src/todo/todo.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import * as z from 'zod'
+import { validateRequest } from 'zod-express-middleware'
 
+import { Todo } from './todo.model'
 import {
 	createTodo,
 	deleteTodo,
@@ -11,11 +14,29 @@ import {
 
 const router = Router()
 
+const body = { body: Todo }
+const paramsWithId = {
+	params: z.object({
+		id: z.string().regex(/[0-9A-Fa-f]{24}/g),
+	}),
+}
+
 router.get('/', getTodos)
-router.get('/:id', getTodoById)
-router.post('/', createTodo)
-router.patch('/:id', updatePartialTodo)
-router.put('/:id', updateTodo)
-router.delete('/:id', deleteTodo)
+router.get('/:id', validateRequest(paramsWithId), getTodoById)
+router.post('/', validateRequest(body), createTodo)
+router.patch(
+	'/:id',
+	validateRequest({ ...paramsWithId, body: Todo.partial() }),
+	updatePartialTodo
+)
+router.put(
+	'/:id',
+	validateRequest({
+		...paramsWithId,
+		...body,
+	}),
+	updateTodo
+)
+router.delete('/:id', validateRequest(paramsWithId), deleteTodo)
 
 export { router }
