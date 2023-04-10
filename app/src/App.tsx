797afbbfa2ccb35c230d084ada8331faecac8d0d
mariefj <marie.forja@gmail.com>
2023-04-10 14:22:26 +0200

    feat(): add eslint config

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+// eslint-disable-next-line import/no-absolute-path
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,16 +10,16 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type='button' onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
         <p>
